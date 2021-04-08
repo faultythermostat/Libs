bb5a9b848ed21bac80da2b5c1452f7474696c4d7
Evan Morschhauser <evanmorsch@gmail.com>
2021-04-08 00:11:43 -0400

    Added some comments

--- drawStuff2d.js
+++ drawStuff2d.js
@@ -1,18 +1,17 @@
 //color scaler function (b-w scale)
+//poly line array drawing function
+//update to class based?..
 
-var c = document.getElementById("Canvas");
+var c = document.getElementById("Canvas");//create global ctx object
 ctx = c.getContext("2d");
 
 ctx.init = function() {
-	//make the size changable as a parameter?
-	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.width = window.innerWidth;//set canvas size
 	ctx.canvas.height = window.innerHeight;
-	SCREENWIDTH = window.innerWidth;
+	SCREENWIDTH = window.innerWidth;//create size global
 	SCREENHEIGHT = window.innerHeight;
-	//camera = new _camera(new _VECTOR3D(0,0,0));
-	return true;
 }
-ctx.changeFontSize = function(ns=12) {
+ctx.changeFontSize = function(ns=12) {//searches for the first non number and replaces to that char with the new size
 	for (var i=0;i<ctx.font.length;i++) {
 		if (isNaN(ctx.font.charAt(i))) {
 			ctx.font = ns+ctx.font.substr(2)
@@ -25,11 +24,10 @@ ctx.strokeCircle = function(x,y,r,color="white") {
 	ctx.strokeStyle = color
 	ctx.stroke()
 }
-ctx.clearScreen = function() {
+ctx.clearScreen = function() {//clears entire screen
 	ctx.clearRect(0,0,SCREENWIDTH,SCREENHEIGHT);
 }
-ctx.setColor = function(col) {
+ctx.setColor = function(col) {//sets all drawing functions to given color
 	ctx.strokeStyle = col
 	ctx.fillStyle = col
 }
-ctx.init()
